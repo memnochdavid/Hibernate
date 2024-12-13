9a4f313a7bad966d1864a6abb82386c6d3d231e4
memnochdavid <davdqueudiaz@gmail.com>
2024-12-13 20:40:42 +0100

    yjygjf

--- src/main/java/org/david/AccesoDatos.java
+++ src/main/java/org/david/AccesoDatos.java
@@ -5,6 +5,7 @@ import org.hibernate.Session;
 import java.util.Collection;
 
 public class AccesoDatos {
+    //1. Buscar los datos del departamento localizado en Dallas.
     static public String DepartamentoInfo(String dep) {//Ejercicio01
         String res = "";
         Session sesion = HibernateUtil.getSessionFactory().openSession();
@@ -14,20 +15,42 @@ public class AccesoDatos {
         Departamento objeto_departamento = (Departamento) sentencia.uniqueResult();
         res+="Nombre Dpto.: "+objeto_departamento.getNombre()+"\n";
         res+="Localizacion: "+objeto_departamento.getLocalizacion()+"\n";
+        sesion.close();
+        return res;
+    }
+
+    //2. Mostrar los empleados que trabajan en el departamento anterior.
+    static public String DepartamentoInfoEmpleados(String dep) {//Ejercicio01
+        String res = "";
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        String consulta = "from Departamento d where d.localizacion='" + dep + "'";
+        Query sentencia = sesion.createQuery(consulta);
+        Departamento objeto_departamento = (Departamento) sentencia.uniqueResult();
+        res+="Nombre Dpto.: "+objeto_departamento.getNombre()+"\n";
+        res+="Localizacion: "+objeto_departamento.getLocalizacion()+"\n";
         res+="Empleados de "+dep+":\n";
-
         Collection<Empleado> empleados = objeto_departamento.getEmpleados();
-
         for (Empleado objeto_empleado : empleados) {
             res += "Apellido: "+objeto_empleado.getApellido() + ", Cargo: "
                     + objeto_empleado.getCargo() + ", Salario: "
                     + objeto_empleado.getSalario() + "\n";
         }
-
-
         sesion.close();
         return res;
     }
 
+    //3. Buscar el empleado con mayor sueldo de toda la empresa.
+    static public String EmpleadoMillonetis(){
+        String res="";
+        Session sesion = HibernateUtil.getSessionFactory().openSession();
+        String consulta = "from Empleado e where salario==max(salario)";
+
+        Query sentencia = sesion.createQuery(consulta);
+        Empleado objeto_millonetis = (Empleado) sentencia.uniqueResult();
+        res+="Empleado más forrado:\n";
+        res+="Apellido: "+objeto_millonetis.getApellido()+", Cargo: "+objeto_millonetis.getCargo()+", Salario: "+objeto_millonetis.getSalario()+"\n";
+
+        return res;
+    }
 
 }
--- src/main/java/org/david/Main.java
+++ src/main/java/org/david/Main.java
@@ -11,6 +11,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ejercicio 1:\n");
         System.out.println(AccesoDatos.DepartamentoInfo("DALLAS"));
+        System.out.println("Ejercicio 2:\n");
+        System.out.println(AccesoDatos.DepartamentoInfoEmpleados("DALLAS"));
+        System.out.println("Ejercicio 3:\n");
+        System.out.println(AccesoDatos.EmpleadoMillonetis());
+
+
     }
 
 
